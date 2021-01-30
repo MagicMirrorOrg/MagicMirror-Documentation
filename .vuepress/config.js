167b50d774d6b56cdd85b4b6b8fbea17663c80b4
buxxi <buxxi@omfilm.net>
2021-01-30 12:59:07 +0100

    Adding a bit of documentation about the custom css

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -45,7 +45,8 @@ module.exports = {
               'modules/weather',
               'modules/weatherforecast'
             ]
-          }
+          },
+          'modules/customcss'
         ]
       },
       {
